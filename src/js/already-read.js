f069c1483162fbbeb2c6b808ac0ef3f477182211
Zhvanko Anna <zhvankoann@gmail.com>
2023-02-05 13:24:32 +0200

    Restructured function read more

--- src/js/already-read.js
+++ src/js/already-read.js
@@ -1,14 +1,14 @@
-let today = new Date();
-const date =
-  today.getDate() < 10
-    ? today.getDate().toString().padStart(2, 0)
-    : today.getDate();
-const month =
-  today.getMonth() < 10
-    ? (today.getMonth() + 1).toString().padStart(2, 0)
-    : today.getMonth() + 1;
-const year = today.getFullYear();
-const dateNow = `${date}/${month}/${year}`;
+// let today = new Date();
+// const date =
+//   today.getDate() < 10
+//     ? today.getDate().toString().padStart(2, 0)
+//     : today.getDate();
+// const month =
+//   today.getMonth() < 10
+//     ? (today.getMonth() + 1).toString().padStart(2, 0)
+//     : today.getMonth() + 1;
+// const year = today.getFullYear();
+// const dateNow = `${date}/${month}/${year}`;
 
 const readWrapperEl = document.querySelector('.read-wrapper');
 
--- src/js/read-more.js
+++ src/js/read-more.js
@@ -8,10 +8,11 @@ function handleLinkClick(e) {
   }
   e.target.parentNode.nextElementSibling.classList.add(`overlay-shown`);
 
-  const readArr = [];
-  const keyArr = [];
-  const idArr = [];
+  let readArr = [];
+  // const keyArr = [];
+  let idArr = [];
   let today = new Date();
+  console.log(today)
   const date =
     today.getDate() < 10
       ? today.getDate().toString().padStart(2, 0)
@@ -22,6 +23,8 @@ function handleLinkClick(e) {
       : today.getMonth() + 1;
   const year = today.getFullYear();
   const dateKey = `${date}/${month}/${year}`;
+  const dateNow = today.getTime();
+  console.log(dateKey)
 
   const newsId = e.target.parentNode.parentNode.children[2].dataset.id;
 
@@ -34,41 +37,57 @@ function handleLinkClick(e) {
     date: e.target.previousElementSibling.textContent,
     originUrl: e.target.href,
     dateKey,
+    dateNow,
   };
 
-  if (localStorage.getItem('id')) {
-    const arrFromLocal = JSON.parse(localStorage.getItem('id'));
-    idArr.push(...arrFromLocal);
-  }
+  // if (localStorage.getItem('id')) {
+  //   const arrFromLocal = JSON.parse(localStorage.getItem('id'));
+  //   idArr.push(...arrFromLocal);
+  // }
+  if (localStorage.getItem('read')) {
+    console.log('readArr', readArr)
 
-  if (idArr.includes(`${newsId}`)) {
-    return;
-  } else {
-    idArr.push(newsId);
-    localStorage.setItem('id', JSON.stringify(idArr));
-
-    e.target.parentNode.parentNode.classList.add('have-read');
-
-    if (localStorage.getItem('read')) {
-      const keysFromLocal = JSON.parse(localStorage.getItem('read'));
-      keyArr.push(...keysFromLocal);
-      if (!keyArr.includes(dateKey)) {
-        keyArr.push(dateKey);
-      }
-      localStorage.setItem('read', JSON.stringify(keyArr));
-    } else {
-      const key = [];
-      key.push(dateKey);
-      localStorage.setItem('read', JSON.stringify(key));
-    }
-
-    if (localStorage.getItem(`${dateKey}`)) {
-      const arrLocalNews = JSON.parse(localStorage.getItem(`${dateKey}`));
-      arrLocalNews.push(newsObj);
-      localStorage.setItem(`${dateKey}`, JSON.stringify(arrLocalNews));
+    readArr = JSON.parse(localStorage.getItem('read'));
+    idArr = readArr.map(item => item.id);
+    console.log('readArr', readArr)
+    console.log('idArr', idArr);
+    if (idArr.includes(`${newsId}`)) {
+      return;
     } else {
       readArr.push(newsObj);
-      localStorage.setItem(`${dateKey}`, JSON.stringify(readArr));
+      localStorage.setItem('read', JSON.stringify(readArr));
     }
+  } else {
+    readArr.push(newsObj);
+    localStorage.setItem('read', JSON.stringify(readArr));
   }
+
+  // else {
+  // idArr.push(newsId);
+  // localStorage.setItem('id', JSON.stringify(idArr));
+
+  //   e.target.parentNode.parentNode.classList.add('have-read');
+
+  //   if (localStorage.getItem('read')) {
+  //     const keysFromLocal = JSON.parse(localStorage.getItem('read'));
+  //     keyArr.push(...keysFromLocal);
+  //     if (!keyArr.includes(dateKey)) {
+  //       keyArr.push(dateKey);
+  //     }
+  //     localStorage.setItem('read', JSON.stringify(keyArr));
+  //   } else {
+  //     const key = [];
+  //     key.push(dateKey);
+  //     localStorage.setItem('read', JSON.stringify(key));
+  //   }
+
+  //   if (localStorage.getItem(`${dateKey}`)) {
+  //     const arrLocalNews = JSON.parse(localStorage.getItem(`${dateKey}`));
+  //     arrLocalNews.push(newsObj);
+  //     localStorage.setItem(`${dateKey}`, JSON.stringify(arrLocalNews));
+  //   } else {
+  //     readArr.push(newsObj);
+  //     localStorage.setItem(`${dateKey}`, JSON.stringify(readArr));
+  //   }
+  // }
 }
